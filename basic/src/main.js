1d3f76b31965938558df5ca3b88cf05d60931845
baomei1996 <qhal4686@gmail.com>
2023-01-19 00:23:14 +0900

    Feat: 기본 장면 만들기 - Camera, Mesh

--- basic/src/main.js
+++ basic/src/main.js
@@ -13,5 +13,41 @@ import * as THREE from "three";
  */
 
 const canvas = document.getElementById("three-canvas");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+/**
+ * Camera
+ * - 시야각 (filed of view)
+ * - 종횡비 (aspect)
+ * - near
+ * - far
+ */
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+);
+
+camera.position.y = 2;
+camera.position.z = 5;
+camera.position.x = 1;
+scene.add(camera);
+
+/**
+ * Mesh
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+renderer.render(scene, camera);
