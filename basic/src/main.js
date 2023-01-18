d648ddceb079a0ecf8227ad3bcd8939bc31f46c9
baomei1996 <qhal4686@gmail.com>
2023-01-19 00:35:21 +0900

    Feat: 직교카메라(Orthographic Camera)

--- basic/src/main.js
+++ basic/src/main.js
@@ -22,21 +22,45 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 /**
  * Camera
+ * (1) PerspectiveCamera
  * - 시야각 (filed of view)
  * - 종횡비 (aspect)
  * - near
  * - far
+ * (2) OrthographicCamera
+ * - left
+ * - right
+ * - top
+ * - bottom
  */
-const camera = new THREE.PerspectiveCamera(
-    75,
+// const camera = new THREE.PerspectiveCamera(
+//     75,
+//     window.innerWidth / window.innerHeight,
+//     0.1,
+//     1000
+// );
+
+// camera.position.y = 2;
+// camera.position.z = 5;
+// camera.position.x = 1;
+// scene.add(camera);
+
+const camera = new THREE.OrthographicCamera(
+    -(window.innerWidth / window.innerHeight),
     window.innerWidth / window.innerHeight,
+    1,
+    -1,
     0.1,
     100
 );
 
-camera.position.y = 2;
 camera.position.z = 5;
 camera.position.x = 1;
+camera.position.y = 2;
+
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
 scene.add(camera);
 
 /**
