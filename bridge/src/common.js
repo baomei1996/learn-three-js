9ec36a56830426e747025e5986ca9155cf3e0688
baomei1996 <qhal4686@gmail.com>
2023-04-27 23:01:21 +0900

    Feat: 기본 장면 구성

---
+++ bridge/src/common.js
@@ -0,0 +1,11 @@
+import { Scene } from "three";
+
+// Canvas에 담아줄 정해진 객체
+export const cm1 = {
+    canvas: document.querySelector("#three-canvas"),
+    scene: new Scene(),
+};
+
+export const cm2 = {
+    backgroundColor: "#321321",
+};
