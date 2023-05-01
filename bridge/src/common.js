29d8aac18f7dfccfafa1752e87efd6208b050ff9
baomei1996 <qhal4686@gmail.com>
2023-05-01 23:44:00 +0900

    Feat: 바닥 깔기

--- bridge/src/common.js
+++ bridge/src/common.js
@@ -9,13 +9,16 @@ export const cm1 = {
 export const cm2 = {
     backgroundColor: "#321321",
     lightColor: "#ffe9ac",
+    floorColor: "#111",
     pillarColor: "#071d28",
 };
 
 export const geo = {
+    floor: new BoxGeometry(200, 1, 200),
     pillar: new BoxGeometry(5, 10, 5),
 };
 
 export const mat = {
+    floor: new MeshPhongMaterial({ color: cm2.floorColor }),
     pillar: new MeshPhongMaterial({ color: cm2.pillarColor }),
 };
