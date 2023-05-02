2e59730c55748ef27b26df8f493581482a29221e
baomei1996 <qhal4686@gmail.com>
2023-05-02 23:02:29 +0900

    Feat: 물리엔진 기본설정

--- bridge/src/main.js
+++ bridge/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import * as CANNON from "cannon-es";
 import { cm1, cm2 } from "./common";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Pillar } from "./Pillar";
@@ -74,6 +75,40 @@ cm1.scene.add(spotLight1, spotLight2, spotLight3, spotLight4);
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+
+// 물리 엔진
+cm1.world.gravity.set(0, -10, 0);
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    cm1.defaultMaterial,
+    cm1.defaultMaterial,
+    {
+        friction: 0.3,
+        restitution: 0.2,
+    }
+);
+
+const glassDefaultContactMaterial = new CANNON.ContactMaterial(
+    cm1.glassMaterial,
+    cm1.defaultMaterial,
+    {
+        friction: 1,
+        restitution: 0,
+    }
+);
+
+const playerGlassContactMaterial = new CANNON.ContactMaterial(
+    cm1.glassMaterial,
+    cm1.playerMaterial,
+    {
+        friction: 1,
+        restitution: 0,
+    }
+);
+
+cm1.world.defaultContactMaterial = defaultContactMaterial;
+cm1.world.addContactMaterial(glassDefaultContactMaterial);
+cm1.world.addContactMaterial(playerGlassContactMaterial);
 
 // 물체 만들기
 const glassUnitSize = 1.2;
